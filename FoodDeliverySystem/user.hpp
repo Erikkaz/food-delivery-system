8bfdfc9b95c9c508194d20170b72899d32518ecc
Erik Kazaryan <erik2007kaz@gmail.com>
2025-10-26 23:36:20 +0700

    Создан базовый класс User

---
+++ FoodDeliverySystem/user.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <string>
+#include <ctime>
+
+// Роль пользователя в системе
+enum class UserRole {
+	Client,
+	Courier,
+	Administrator
+};
+
+// Базовый класс пользователя
+class User
+{
+private:
+	static unsigned int nextId;
+	unsigned int id;
+	std::string passwordHash;
+	std::string login;
+	std::time_t registrationDate;
+	std::string fullName;
+
+protected:
+	std::string email;
+	std::string phone;
+	bool isActive;
+	UserRole role;
+
+public:
+	User();
+	User(const User& other);
+	virtual ~User();
+
+	int getId() const;
+	std::string getLogin() const;
+	std::time_t getRegistrationDate() const;
+	std::string getFullName() const;
+	std::string getEmail() const;
+	std::string getPhone() const;
+	bool getIsActive() const;
+	UserRole getRole() const;
+
+	void setFullName(const std::string& name);
+	void setEmail(const std::string& email);
+	void setPhone(const std::string& phone);
+	void setPassword(const std::string& password);
+	void setLogin(const std::string& login);
+
+	void activate();
+	void deactivate();
+
+	bool authenticate(const std::string& inputPassword) const;
+	void changePassword(const std::string& oldPassword, const std::string& newPassword);
+
+	virtual void displayInfo() const = 0;  // Вывод информации о пользователе
+
+	virtual std::string getUserType() const = 0;  // Возвращает роль пользователя
+
+private:
+	std::string hashPassword(const std::string& password) const;
+};
