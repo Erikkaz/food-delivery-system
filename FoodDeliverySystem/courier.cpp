6fad94aeaf48f943016c6ab435fa0aec0d4fdb07
Erik Kazaryan <erik2007kaz@gmail.com>
2025-11-05 00:33:17 +0700

    Добавлен класс Courier

---
+++ FoodDeliverySystem/courier.cpp
@@ -0,0 +1,145 @@
+﻿#include "courier.hpp"
+#include <iostream>
+
+Courier::Courier()
+	: User(),
+	passport(""),
+	contactInfo(""),
+	snils(""),
+	deliveredOrders(),
+	currentOrder(nullptr),  // Курьер изначально свободен
+	rating(0),
+	approvedByAdmin(false)
+{
+
+}
+
+Courier::~Courier()
+{
+
+}
+
+void Courier::provideDocuments(const std::string& passport, const std::string& snils, const std::string& contactInfo)
+{
+	// Курьер передаёт свои документы 
+	this->passport = passport;
+	this->snils = snils;
+	this->contactInfo = contactInfo;
+	std::cout << "Документы успешно переданы администратору!" << std::endl;
+}
+
+void Courier::setApproved(bool state)
+{
+	// Администратор устанавливает, допущен ли курьер к работе.
+	approvedByAdmin = state;
+}
+
+bool Courier::isApproved() const
+{
+	return approvedByAdmin;
+}
+
+void Courier::registerCourier(const std::string& login, const std::string& password) 
+{
+	// Проверка одобрен ли курьер
+	if (!isApproved())
+	{
+		std::cout << "Ошибка: Курьер не одобрен администратором!" << std::endl;
+		return;
+	}
+
+	std::cout << "=== РЕГИСТРАЦИЯ КУРЬЕРА ===" << std::endl;
+	registerUser(login,password);
+}
+
+
+double Courier::getTotalEarnings() const
+{
+	// Вычисляем общую прибыль курьера
+	double total = 0;
+	for (const auto& order : deliveredOrders)
+	{
+		total += order.getCourierProfit();
+	}
+
+	return total;
+}
+
+void Courier::viewEarnings() const
+{
+	std::cout << "\n=== ДОХОДЫ КУРЬЕРА ===\n";
+
+	if (deliveredOrders.empty())
+	{
+		std::cout << "Нет выполненных заказов. \n";
+		return;
+	}
+
+	int index = 1;  // переменная для нумерации заказа
+
+	for (const auto& order : deliveredOrders) {
+		std::cout << "Заказ №" << index << " | Заработано: " << order.getCourierProfit() << " руб." << std::endl;
+		index++;
+	}
+
+	std::cout << "---------------------------" << std::endl;
+	std::cout << "ИТОГО: " << getTotalEarnings() << " руб. \n\n";
+}
+
+bool Courier::acceptOrder(Order* order)
+{
+	// Проверяем, пустой ли заказ
+	if (order == nullptr)
+	{
+		std::cout << "Ошибка: заказ не найден.\n";
+		return false;
+	}
+
+	// Проверяем, свободен ли курьер
+	if (currentOrder != nullptr)
+	{
+		std::cout << "Невозможно принять заказ: курьер уже выполняет другой.\n";
+		return false;
+	}
+
+	// Заказ должен быть готов к выдаче
+	if (order->getStatus() != OrderStatus::Ready_for_pickup)
+	{
+		std::cout << "Заказ еще не готов или уже принят/доставлен.\n";
+		return false;
+	}
+
+	// Курьер принимает заказ
+	order->setStatus(OrderStatus::On_the_way);
+	currentOrder = order;
+
+	return true;
+}
+
+bool Courier::completeCurrentOrder()
+{
+	if (currentOrder == nullptr)
+	{
+		std::cout << "Нет активного заказа у курьера." << std::endl;
+		return false;
+	}
+
+	// Проверяем статус
+	if (currentOrder->getStatus() != OrderStatus::On_the_way)
+	{
+		std::cout << "Невозможно завершить заказ: он еще не в пути." << std::endl;
+		return false;
+	}
+
+	// Помечаем заказ как доставленный
+	currentOrder->setStatus(OrderStatus::Delivered);
+
+	// Добавляем в список завершённых 
+	deliveredOrders.push_back(*currentOrder);
+
+	// Курьер становится свободен  (Обнуляем указатель)
+	currentOrder = nullptr;
+
+	std::cout << "Заказ успешно доставлен.\n";
+	return true;
+}
---
+++ FoodDeliverySystem/courier.hpp
@@ -0,0 +1,39 @@
+﻿#include <string>
+#include <vector>
+#include "user.hpp"
+#include "order.hpp"
+#include "review.hpp"
+
+class Courier : public User
+{
+private:
+	// Данные курьера, которые необходимо предоставить администратору
+	std::string passport;
+	std::string contactInfo;
+	std::string snils;
+
+	std::vector<Order> deliveredOrders;  // Список выполненных заказов
+	Order* currentOrder;  // Текущий заказ
+
+	float rating;  // Рейтинг курьера
+	bool approvedByAdmin;  // Одобрен ли администратором
+
+public:
+	Courier();
+	~Courier();
+	
+	// Передача документов администратору
+	void provideDocuments(const std::string& passport, const std::string& snils, const std::string& contact);
+
+	void setApproved(bool state);    // Установка флага "одобрен"
+	bool isApproved() const;   // Решение администратора
+	bool completeCurrentOrder();  // Завершение текущего заказа
+
+	void registerCourier(const std::string& login, const std::string& password);  // Регистрация
+	double getTotalEarnings() const;   // Общая прибыль
+
+	bool acceptOrder(Order* order);   // Принять заказ
+	//Order* viewAvailableOrders();
+	//Review* viewReviews(); 
+	void viewEarnings() const;    // Посмотреть заработок
+};
