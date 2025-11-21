b0242b6a31b5c5ac97b877063ff51679804f23a1
Erik Kazaryan <erik2007kaz@gmail.com>
2025-11-21 20:37:44 +0700

    Добавлен класс Order

---
+++ FoodDeliverySystem/order.hpp
@@ -0,0 +1,41 @@
+﻿#pragma once
+#include <ctime>
+#include <string>
+
+// Статусы заказа
+enum class OrderStatus
+{
+    Cooking,      // Готовится
+    Ready_for_pickup, // Готов к выдаче/курьеру 
+    On_the_way,   // В пути
+    Delivered,    // Доставлен
+    Cancelled,    // Отменен
+    Not_found     // Не найден
+};
+
+class Order
+{
+private:
+    static unsigned int nextId; // для автоматической генерации id
+	unsigned int id;  // id заказа
+    double price;  // сумма заказа
+    double courierProfit;  // Прибыль курьера
+    std::time_t orderTime;  // Время создания заказа
+    std::string clientName;  // Имя клиента, оформившего заказ
+    OrderStatus status;   // Статус заказа
+
+public:
+    Order();   // Конструктор по умолчанию
+    Order(int id, double price);  // Конструктор с Id и ценой
+    ~Order();  // Деструктор
+
+    void setStatus(OrderStatus newStatus);  // Установить статус заказа
+
+    // === Геттеры === 
+    unsigned int getId() const;
+    double getPrice() const;
+    OrderStatus getStatus() const;
+    double getCourierProfit() const;
+    std::time_t getOrderTime() const;
+    std::string getClientName() const;
+};
